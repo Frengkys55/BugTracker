b99819bde9064a4f805ae279c0dd2ce4a718883e
FrengkySinaga <33502834+Frengkys55@users.noreply.github.com>
2023-05-08 01:08:16 +0000

    Adding some code to Comment component

---
+++ Models/Comments/CommentsProperties.cs
@@ -0,0 +1,12 @@
+using Models;
+
+namespace Models.Comments;
+
+public partial class Comment{
+    public string? id {set; get;}
+    public Guid guid {set; get;}
+    public Guid TicketGuid {set; get;}
+    public Guid UserGuid {set; get;}
+    public string? CommentText {set; get;}
+    public Comments.Attachment Attachment {set; get;}
+}
---
+++ Models/Comments/TicketCommentAttachment.cs
@@ -0,0 +1,31 @@
+namespace Models.Comments;
+
+/// <summary>
+/// Attachment model for the comment class
+/// </summary>
+public partial class Attachment{
+    /// <summary>
+    /// Id of the attachment from database
+    /// </summary>
+    public int id {set; get;}
+    /// <summary>
+    /// Guid of the attachment
+    /// </summary>
+    public Guid guid {set; get;}
+    /// <summary>
+    /// Comment guid this attachment is a part of
+    /// </summary>
+    public Guid CommentGuid {set; get;}
+    /// <summary>
+    /// Guid of the attachment type
+    /// </summary>
+    public Guid TypeGuid {set; get;}
+    /// <summary>
+    /// Name of the attachment
+    /// </summary>
+    public string? Name {set; get;}
+    /// <summary>
+    /// Relative location of the attachment
+    /// </summary>
+    public string? Location {set; get;}
+}
