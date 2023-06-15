ebe457acaf6a19a0fea2fdcc78ce2fb1067c7260
FrengkySinaga <33502834+Frengkys55@users.noreply.github.com>
2023-06-15 03:43:28 +0000

    Partial implementation of user authentication

--- Models/Authencations/Login/UserLoginModelMethods.cs
+++ Models/Authencations/Login/UserLoginModelMethods.cs
@@ -1,10 +1,34 @@
+using System.Security.Cryptography;
+using System.Text;
+using Tools.Misc;
+
 namespace Models{
     public partial class UserLoginModel{
+        private readonly AccessTokenHelper _accesstokenHelper;
+        public UserLoginModel(AccessTokenHelper _accesstokenHelper){
+            this._accesstokenHelper = _accesstokenHelper;
+        }
+
         public string Login(UserLoginModel user){
-            throw new NotImplementedException();
+            // Hash username and password
+            string hashedUsername;
+            string hashedPassword;
+
+            var sha512 = SHA512.Create();
+            hashedUsername = Convert.ToBase64String(sha512.ComputeHash(Encoding.UTF8.GetBytes(user.UserName)));
+            hashedPassword = Convert.ToBase64String(sha512.ComputeHash(Encoding.UTF8.GetBytes(user.password)));
+            Console.WriteLine(hashedPassword);
+            Console.WriteLine(hashedUsername);
+
+            return "58d18562-5ed6-4da2-95db-777ab7dd422a";
         }
         public bool CheckLogin(string accesstoken){
             throw new NotImplementedException();
+        }
+        
+        public bool Logout(string accesstoken){
+            _accesstokenHelper.SetAccesstoken(string.Empty);
+            return true;
         }
     }
 }
--- Tools/Misc/AccessTokenHelper.cs
+++ Tools/Misc/AccessTokenHelper.cs
@@ -1,10 +1,17 @@
 namespace Tools.Misc{
     public sealed class AccessTokenHelper{
-        private string _accessToken = "58d18562-5ed6-4da2-95db-777ab7dd422a";
+        private string _accessToken = string.Empty;
+        //private string _accessToken = "58d18562-5ed6-4da2-95db-777ab7dd422a";
         public string accessToken { get{
             return _accessToken;
-        } set{
+        } private set{
             _accessToken = value;
         }}
+
+        public void SetAccesstoken(string accesstoken){
+            _accessToken = accesstoken;
+        }
     }
+
+
 }
