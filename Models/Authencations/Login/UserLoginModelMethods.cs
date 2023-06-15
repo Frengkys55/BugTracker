b5bd520e699f70c7aa8907fe0eff55885c3d0a4f
FrengkySinaga <33502834+Frengkys55@users.noreply.github.com>
2023-06-15 07:39:19 +0000

    Implementing latest created project

--- Models/Authencations/Login/UserLoginModelMethods.cs
+++ Models/Authencations/Login/UserLoginModelMethods.cs
@@ -8,8 +8,11 @@ namespace Models{
         public UserLoginModel(AccessTokenHelper _accesstokenHelper){
             this._accesstokenHelper = _accesstokenHelper;
         }
+        public UserLoginModel(){
 
-        public string Login(UserLoginModel user){
+        }
+
+        public async Task<string> Login(UserLoginModel user, string address){
             // Hash username and password
             string hashedUsername;
             string hashedPassword;
@@ -17,8 +20,14 @@ namespace Models{
             var sha512 = SHA512.Create();
             hashedUsername = Convert.ToBase64String(sha512.ComputeHash(Encoding.UTF8.GetBytes(user.UserName)));
             hashedPassword = Convert.ToBase64String(sha512.ComputeHash(Encoding.UTF8.GetBytes(user.password)));
-            Console.WriteLine(hashedPassword);
-            Console.WriteLine(hashedUsername);
+
+            // Send user login information and get accesstoken
+            try{
+                var result = await new Tools.APIHelper.GenericRequest().Send2<UserLoginModel>(Tools.APIHelper.SendMethod.POST, address, user);
+            }
+            catch(Exception){
+                throw;
+            }
 
             return "58d18562-5ed6-4da2-95db-777ab7dd422a";
         }
