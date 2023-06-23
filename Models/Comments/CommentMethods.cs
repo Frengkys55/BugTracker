db8384021a395eeab8a0bd5e310fe3370ef841f0
frengkys55 <frengkysinaga@windowslive.com>
2023-06-23 15:23:01 +0900

    Implemeted comment deletion method

--- Models/Comments/CommentMethods.cs
+++ Models/Comments/CommentMethods.cs
@@ -11,19 +11,46 @@ public partial class Comment{
         GenericRequest request = new GenericRequest();
         try{
             var result = await request.Send2<Comment>(SendMethod.POST, address, comment, headers);
-            Console.WriteLine("Comment post result: " + result.Content.ReadAsStringAsync());
+            Models.ResponseMessage responseMessage = new Tools.Misc.JsonConverter<ResponseMessage>().ReadString(await result.Content.ReadAsStringAsync());
+            if(responseMessage.statusCode == 201)
+                return 1;
+            else
+                return -1;
         }
-        catch(Exception err){
-            Console.WriteLine(err.Message);
+        catch(Exception){
+            throw;
         }
-        throw new NotImplementedException();
     }
 
     public async Task<ICollection<Comment>> GetComments(Guid ticketGuid, string address, string accesstoken){
-        throw new NotImplementedException();
+        var headers = new List<KeyValuePair<string, string>>{
+            new KeyValuePair<string, string>("accesstoken", accesstoken),
+        };
+
+        try{
+            var result = await new Tools.APIHelper.GenericRequest().Send2<DBNull>(SendMethod.GET, address, null, headers);
+            return new Tools.Misc.JsonConverter<List<Comment>>().ReadString(await result.Content.ReadAsStringAsync());
+        }
+        catch(Exception err){
+            Console.WriteLine(err.Message);
+            throw;
+        }
     }
 
     public async Task<int> DeleteComment(Guid commentGuid, string address, string accesstoken){
-        throw new NotFiniteNumberException();
+        var headers = new List<KeyValuePair<string, string>>(){
+            new KeyValuePair<string, string>("accesstoken", accesstoken)
+        };
+
+        try{
+            var result = await new GenericRequest().Send(SendMethod.DELETE, address, headers);
+            if(result == 1)
+                return result;
+            else
+                return -1;
+        }
+        catch(Exception){
+            throw;
+        }
     }
 }
