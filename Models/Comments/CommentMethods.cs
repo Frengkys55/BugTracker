4fcc0c72733497cde8667a4ee86a9bab690e4a5b
frengkys55 <frengkysinaga@windowslive.com>
2023-07-14 13:33:57 +0900

    Cleaning comments related things

--- Models/Comments/CommentMethods.cs
+++ Models/Comments/CommentMethods.cs
@@ -8,14 +8,13 @@ public partial class Comment{
         var headers = new List<KeyValuePair<string, string>>(){
             new KeyValuePair<string, string>("accesstoken", accesstoken)
         };
+
         GenericRequest request = new GenericRequest();
         try{
             var result = await request.Send2<Comment>(SendMethod.POST, address, comment, headers);
             Models.ResponseMessage responseMessage = new Tools.Misc.JsonConverter<ResponseMessage>().ReadString(await result.Content.ReadAsStringAsync());
-            if(responseMessage.statusCode == 201)
-                return 1;
-            else
-                return -1;
+            if(responseMessage.statusCode == 201) return 1;
+            else return -1;
         }
         catch(Exception){
             throw;
@@ -32,7 +31,6 @@ public partial class Comment{
             return new Tools.Misc.JsonConverter<List<Comment>>().ReadString(await result.Content.ReadAsStringAsync());
         }
         catch(Exception err){
-            Console.WriteLine(err.Message);
             throw;
         }
     }
@@ -44,10 +42,8 @@ public partial class Comment{
 
         try{
             var result = await new GenericRequest().Send(SendMethod.DELETE, address, headers);
-            if(result == 1)
-                return result;
-            else
-                return -1;
+            if(result == 1) return result;
+            else return -1;
         }
         catch(Exception){
             throw;
