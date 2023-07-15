3d3ae9c9e2fdc76b0d8ccd038039dc099508f358
frengkys55 <frengkysinaga@windowslive.com>
2023-07-15 11:27:41 +0900

    Fixed successful same username registration

--- Models/Users/UserMethods.cs
+++ Models/Users/UserMethods.cs
@@ -14,17 +14,15 @@ public partial class User{
     public async Task<int> CreateUser(UserRegisterModel user, string address){
         Tools.APIHelper.GenericRequest request = new Tools.APIHelper.GenericRequest();
         try{
-            Console.WriteLine("Create user triggered");
             var result = await request.Send2<UserRegisterModel>(Tools.APIHelper.SendMethod.POST, address, user, null);
             var response = new Tools.Misc.JsonConverter<Models.ResponseMessage>().ReadString(await result.Content.ReadAsStringAsync());
             if(response.statusCode == 201)
                 return 1;
             else
-                return -1;
+                throw new HttpRequestException(response.reasonPhrase);
         }
-        catch(Exception err){
-            Console.WriteLine(err.Message);
-            return -1;
+        catch(Exception){
+            throw;
         }
     }
 
